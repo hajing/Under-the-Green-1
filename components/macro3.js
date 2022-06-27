385ea079fa25ff1af19e3203ab79e3bd914d0ff0
hejing <hejing@comtom.cn>
2022-06-27 20:34:15 +0800

    feat: macro3背景图

--- components/macro3.js
+++ components/macro3.js
@@ -1,5 +1,6 @@
 let loadMacro3 = 0;
 const maxMacro3 = 1;
+let isFinish = false;
 
 function initMacro3() {
   loadMacro3++;
@@ -11,9 +12,20 @@ function initMacro3() {
   registerScroll("#svg_macro3", (event, isDown) => {
     console.log(isDown);
     if (isDown) {
-      scrollTo(6, 1000, false);
-      setTimeout(() => showLight(), 500);
-      initAnimMicro1();
+      //
+      if (!isFinish) {
+        // 未完成滚动
+        // 1 左边文字变化 general trends 图片变化
+        // 2 左边文字变化  2004? 图片变化
+        // 3 左边文字变化  2008 图片变化
+        // 4 左边文字变化  fall in 2004 图片变化
+        // 5 左边文字变化  fall in 2004 图片变化
+        // 6
+      } else {
+        scrollTo(6, 1000, false);
+        setTimeout(() => showLight(), 500);
+        initAnimMicro1();
+      }
     } else {
       scrollTo(4, 1000, false);
       d3.select("#macro2_audio")
